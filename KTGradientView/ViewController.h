b79a775f27db6fcc3c27a8b5a8f8c0240a69a993
Kevin Tu <819931323@qq.com>
2015-12-16 20:37:29 +0800

    Initial Commit

---
+++ KTGradientView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTGradientView
+//
+//  Created by tujinqiu on 15/12/16.
+//  Copyright © 2015年 tujinqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
